d48ebac11a6200c60fec3c74c19f7467f2d22104
franzosterkamp <franz.osterkamp@gmx.net>
2019-11-26 19:18:18 +0100

    add html forms

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -4,13 +4,14 @@ function CardForm() {
   return (
     <form>
       <label>
-        <input type="text" />
+        <input type="text" required />
       </label>
       <label>
-        <input type="date" />
+        <input type="date" required />
       </label>
-      <textarea rows="10"></textarea>
+      <textarea rows="10" required></textarea>
       <button>Submit</button>
+      <button type="reset">Reset</button>
     </form>
   );
 }
